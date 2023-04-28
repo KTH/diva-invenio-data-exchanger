f551aac641419d0d08d0223c17bee7b8aa6a06d6
Sam Al Arbid <samaa2@kth.se>
2023-04-28 20:05:35 +0200

    fix: swedish chars

--- diva_invenio_data_exchanger/converter.py
+++ diva_invenio_data_exchanger/converter.py
@@ -1,3 +1,5 @@
+import json
+
 import pandas as pd
 
 
@@ -5,4 +7,4 @@ def csv_to_json(csv_path, json_path):
     """CSV_to_JSON."""
     df = pd.read_csv(csv_path)
     with open(json_path, "w", encoding="utf-8") as f:
-        f.write(df.to_json(orient="records", indent=4))
+        f.write(df.to_json(orient="records", indent=4, force_ascii=False))
