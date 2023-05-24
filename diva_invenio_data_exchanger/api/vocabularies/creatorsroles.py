a122b1ff53b5cda769df7c47bc5a01e66e8ffb13
Sam Al Arbid <samaa2@kth.se>
2023-05-24 16:08:36 +0200

    add creatorsroles_values for ref

---
+++ diva_invenio_data_exchanger/api/vocabularies/creatorsroles.py
@@ -0,0 +1,294 @@
+creatorsroles_values = [
+    "registrationauthority"
+    "registrationagency"
+    "hostinginstitution"
+    "workpackageleader"
+    "projectmanager"
+    "projectleader"
+    "projectmember"
+    "relatedperson"
+    "researchgroup"
+    "contactperson"
+    "datacollector"
+    "rightsholder"
+    "datacurator"
+    "datamanager"
+    "distributor"
+    "supervisor"
+    "researcher"
+    "producer"
+    "sponsor"
+    "editor"
+    "other"
+]
+
+
+json_res = {
+    "hits": {
+        "hits": [
+            {
+                "revision_id": 1,
+                "props": {"datacite": "ContactPerson"},
+                "title": {"en": "Contact person", "de": "Kontaktperson"},
+                "id": "contactperson",
+                "updated": "2023-05-15T21:45:05.809081+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.767418+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/contactperson"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "DataCollector"},
+                "title": {"en": "Data collector", "de": "DatensammlerIn"},
+                "id": "datacollector",
+                "updated": "2023-05-15T21:45:05.820585+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.779609+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/datacollector"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "DataCurator"},
+                "title": {"en": "Data curator", "de": "DatenkuratorIn"},
+                "id": "datacurator",
+                "updated": "2023-05-15T21:45:05.811496+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.770067+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/datacurator"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "DataManager"},
+                "title": {"en": "Data manager", "de": "DatenmanagerIn"},
+                "id": "datamanager",
+                "updated": "2023-05-15T21:45:05.818039+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.783241+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/datamanager"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "Distributor"},
+                "title": {"en": "Distributor", "de": "VerteilerIn"},
+                "id": "distributor",
+                "updated": "2023-05-15T21:45:05.814872+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.779986+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/distributor"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "Editor"},
+                "title": {"en": "Editor", "de": "EditorIn"},
+                "id": "editor",
+                "updated": "2023-05-15T21:45:05.809781+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.779595+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/editor"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "HostingInstitution"},
+                "title": {
+                    "en": "Hosting institution",
+                    "de": "Bereitstellende Institution",
+                },
+                "id": "hostinginstitution",
+                "updated": "2023-05-15T21:45:05.815253+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.782266+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/hostinginstitution"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "Other"},
+                "title": {"en": "Other", "de": "Andere"},
+                "id": "other",
+                "updated": "2023-05-15T21:45:05.988873+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.965837+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/other"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "Producer"},
+                "title": {"en": "Producer", "de": "ProduzentIn"},
+                "id": "producer",
+                "updated": "2023-05-15T21:45:05.813061+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.782996+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/producer"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "ProjectLeader"},
+                "title": {"en": "Project leader", "de": "ProjektleiterIn"},
+                "id": "projectleader",
+                "updated": "2023-05-15T21:45:05.824816+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.787637+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/projectleader"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "ProjectManager"},
+                "title": {"en": "Project manager", "de": "ProjektmanagerIn"},
+                "id": "projectmanager",
+                "updated": "2023-05-15T21:45:05.821674+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.793152+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/projectmanager"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "ProjectMember"},
+                "title": {"en": "Project member", "de": "Projektmitglied"},
+                "id": "projectmember",
+                "updated": "2023-05-15T21:45:05.828661+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.802972+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/projectmember"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "RegistrationAgency"},
+                "title": {"en": "Registration agency", "de": "Registrierungsstelle"},
+                "id": "registrationagency",
+                "updated": "2023-05-15T21:45:05.899410+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.883746+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/registrationagency"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "RegistrationAuthority"},
+                "title": {
+                    "en": "Registration authority",
+                    "de": "Registrierungsbehörde",
+                },
+                "id": "registrationauthority",
+                "updated": "2023-05-15T21:45:05.982665+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.963554+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/registrationauthority"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "RelatedPerson"},
+                "title": {"en": "Related person", "de": "Verwandte Person"},
+                "id": "relatedperson",
+                "updated": "2023-05-15T21:45:05.989731+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.969857+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/relatedperson"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "ResearchGroup"},
+                "title": {"en": "Research group", "de": "Forschungsgruppe"},
+                "id": "researchgroup",
+                "updated": "2023-05-15T21:45:05.982271+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.964141+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/researchgroup"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "Researcher"},
+                "title": {"en": "Researcher", "de": "WissenschaftlerIn"},
+                "id": "researcher",
+                "updated": "2023-05-15T21:45:05.991040+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.964970+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/researcher"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "RightsHolder"},
+                "title": {"en": "Rights holder", "de": "RechteinhaberIn"},
+                "id": "rightsholder",
+                "updated": "2023-05-15T21:45:05.982946+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.964776+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/rightsholder"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "Sponsor"},
+                "title": {"en": "Sponsor", "de": "Sponsor"},
+                "id": "sponsor",
+                "updated": "2023-05-15T21:45:05.991588+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.964168+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/sponsor"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "Supervisor"},
+                "title": {"en": "Supervisor", "de": "SupervisorIn"},
+                "id": "supervisor",
+                "updated": "2023-05-15T21:45:05.998996+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.968513+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/supervisor"
+                },
+            },
+            {
+                "revision_id": 1,
+                "props": {"datacite": "WorkPackageLeader"},
+                "title": {"en": "Work package leader", "de": "ArbeitspaketleiterIn"},
+                "id": "workpackageleader",
+                "updated": "2023-05-15T21:45:05.990549+00:00",
+                "type": "creatorsroles",
+                "created": "2023-05-15T21:45:05.971370+00:00",
+                "links": {
+                    "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles/workpackageleader"
+                },
+            },
+        ],
+        "total": 21,
+    },
+    "sortBy": "title",
+    "links": {
+        "self": "https://127.0.0.1:5000/api/vocabularies/creatorsroles?page=1&size=25&sort=title"
+    },
+}
