894fb0693ee7cd4d71cc5d2d66bd48399537bd8a
Sam Al Arbid <samaa2@kth.se>
2023-05-25 16:55:18 +0200

    refacrtor tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,21 @@
 # pylint: disable=unused-import
 import json
 import os
-import sys
 from unittest.mock import patch
 
 import pytest
 
 from diva_invenio_data_exchanger.utils.general import parse_arguments
-from tests.fixtures.output_fixtures import clear_output_file
 from main import main
-import os
+from tests.fixtures.output_fixtures import clear_output_file
 
 # Get the absolute path of the directory containing the test file
 current_dir = os.path.dirname(os.path.abspath(__file__))
+fixtures_dir = os.path.join(current_dir, "fixtures")
+
+# Define the input file path
+input_file = os.path.join(fixtures_dir, "input/fixture.csv")
+output_file = os.path.join(fixtures_dir, "output/fixture.json")
 
 
 def test_main_raises_error_without_arguments():
@@ -24,9 +27,6 @@ def test_main_raises_error_without_arguments():
 @pytest.mark.usefixtures("clear_output_file")
 def test_main_runs_without_errors():
     """Test that main() runs without raising any exceptions when arguments passed."""
-    input_file = os.path.join(current_dir, "fixtures/input/fixture.csv")
-    output_file = os.path.join(current_dir, "fixtures/output/fixture.json")
-
     with patch("sys.argv", ["prog_name", input_file, output_file]):
         # This will fail the test if main() raises an exception
         main()
@@ -35,9 +35,6 @@ def test_main_runs_without_errors():
 @pytest.mark.usefixtures("clear_output_file")
 def test_main_creates_output_file():
     """Test that main() creates an output file."""
-    input_file = os.path.join(current_dir, "fixtures/input/fixture.csv")
-    output_file = os.path.join(current_dir, "fixtures/output/fixture.json")
-
     with patch("sys.argv", ["prog_name", input_file, output_file]):
         main()
         assert os.path.exists(output_file), "Output file was not created"
@@ -46,9 +43,6 @@ def test_main_creates_output_file():
 @pytest.mark.usefixtures("clear_output_file")
 def test_main_output_file_contains_json_array():
     """Test that the output file created by main() contains a JSON array."""
-    input_file = os.path.join(current_dir, "fixtures/input/fixture.csv")
-    output_file = os.path.join(current_dir, "fixtures/output/fixture.json")
-
     with patch("sys.argv", ["prog_name", input_file, output_file]):
         main()
         with open(output_file, "r", encoding="utf-8") as f:
